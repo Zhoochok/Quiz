7e4dc711ce8fbc0b435928434cb1ddeed91c17e4
Жуков Иван <152768804+Zhoochok@users.noreply.github.com>
2024-02-09 11:59:22 +0300

    createQestions

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-function addd() {}
-function ass() {}
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      name: 'faveReptile',
+
+      message: 'What is your favorite reptile? 🤑',
+
+      default: 'Alligators',
+    },
+
+    {
+      name: 'faveColor',
+
+      message: 'What is your favorite color?',
+
+      default: '#008f68',
+    },
+  ])
+
+  .then((answers) => {
+    console.info('Answers:', answers);
+  });
